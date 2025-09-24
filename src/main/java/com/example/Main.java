d3d8cc310bb0824691d07ae79fea9bef9ad3e5da
Linus Westling <Linus.westling@gmail.com>
2025-09-24 11:06:29 +0200

    Lagt med val av dag (idag/imorgon)

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,41 +3,50 @@ package com.example;
 import com.example.api.ElpriserAPI;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("___________ Testar Elpriser API _____________");
         Scanner scanner = new Scanner(System.in);
         ElpriserAPI elpriserAPI = new ElpriserAPI();
 
-        System.out.println("___________ Testar Elpriser API _____________");
 
         //Frågar användaren om vilket område man vill hämta priser ifrån
-        System.out.println("Vilket område skulle du vilja hämta priser ifrån? (--zone alla|SE1|SE2|SE3|SE4): ");
+        System.out.println("Vilket område skulle du vilja hämta priser ifrån? (alla|SE1|SE2|SE3|SE4): ");
+        String valAvPrisKlass = scanner.nextLine().trim().toUpperCase();
 
-        //Validerar kommando/input från användaren i terminalen
-        if (args.length < 2 || !args[0].equals("--zone")){
-            System.out.println("Ej förväntat argument. Använd \n --zone alla|SE1|SE2|SE3|SE4");
-            return;
+        // Skapa en variabel för vilken dag man vill hämta priser
+        LocalDate idag = LocalDate.now();
+        LocalDate datum;
+        System.out.println("Vill du hämta priser för idag eller imorgon? (idag/imorgon)");
+        String vilkenDag = scanner.nextLine().trim().toUpperCase();
+
+        // Kolla vilken dag användaren har valt
+        if (vilkenDag.equals("IDAG")){
+            datum = idag;
+        }else if (vilkenDag.equals("IMORGON")) {
+            datum = idag.plusDays(1);
+        } else {
+            System.out.println("Ogiltigt datum, default är dagens datum");
+            datum = idag;
         }
 
-        // Sparar det andra argumentet av inmatningen i en sträng för att köra hämtning och display
-        String valAvPrisKlass = args[1].toUpperCase();
 
-        // Skapa en variabel för att hämta dagens priser
-        LocalDate idag = LocalDate.now();
+        System.out.println("Påbörja laddning"); // Behövs vara med enligt testet????
 
 
         // IF ---- Enhanced loop for att få fram alla områden
         // ELSE --- framtagning av pris för specifikt område
-        if(valAvPrisKlass.equals("-- ZONE ALLA")){
+        if(valAvPrisKlass.equals("ALLA")){
             for(ElpriserAPI.Prisklass klass : ElpriserAPI.Prisklass.values()){
-                List<ElpriserAPI.Elpris> allaDagensPriser = elpriserAPI.getPriser(idag, klass);
+                List<ElpriserAPI.Elpris> allaDagensPriser = elpriserAPI.getPriser(datum, klass);
 
                 if (allaDagensPriser.isEmpty()) {
-                    System.out.println("Kunde inte hämta några priser för idag i område: " + klass);
+                    System.out.println("Kunde inte hämta några priser för " + datum + " i område: " + klass);
                 } else {
                     System.out.println("\nDagens elpriser för " + klass + " (" + allaDagensPriser.size() + " st värden):");
                     // Skriv bara ut de 3 första för att hålla utskriften kort
@@ -50,10 +59,10 @@ public class Main {
         } else {
             try{
                 ElpriserAPI.Prisklass valdKlass = ElpriserAPI.Prisklass.valueOf(valAvPrisKlass);
-                List<ElpriserAPI.Elpris> dagensPriser = elpriserAPI.getPriser(idag, valdKlass);
+                List<ElpriserAPI.Elpris> dagensPriser = elpriserAPI.getPriser(datum, valdKlass);
 
                 if (dagensPriser.isEmpty()) {
-                    System.out.println("Kunde inte hämta några priser för idag i område: " + valdKlass);
+                    System.out.println("Kunde inte hämta några priser för " + datum + " i område: " + valdKlass);
                 } else {
                     System.out.println("\nDagens elpriser för " + valdKlass + " (" + dagensPriser.size() + " st värden):");
                     // Skriv bara ut de 3 första för att hålla utskriften kort
